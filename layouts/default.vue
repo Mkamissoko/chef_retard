fa8ce89f4fb4239dcaf123807f64724577486899
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2023-12-10 18:28:03 +0100

    init sitemap

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import Navbar from "~/layouts/Navbar.vue";
+import Footer from "~/layouts/Footer.vue";
+</script>
+
+<template>
+    <Navbar />
+    <slot />
+    <Footer />
+</template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    '@nuxtjs/tailwindcss',
+    'nuxt-simple-sitemap'
+  ],
+  site: {
+    url: "https://chefpasderetard.fr",
+  },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api'
+    }
+  }
 })
